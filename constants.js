f7fa9aeac911cc85676fc43f3f4ddeba57924375
Cephas Cardozo <cephascardozo0207@gmail.com>
2021-12-31 11:09:43 +0530

    Initial Commit

---
+++ constants.js
@@ -0,0 +1,104 @@
+// Options the user could type in
+const prompts = [
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["haha","ha","lol","hehe","funny","joke"],
+    [
+      "kiss me",
+      "you are cute",
+      "is marvel or dc the best"
+    ],
+    ["i had a bad day a school"],
+    ["joke"],
+    ["tell me more" , "more", "say more"]
+    
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok","I understand","What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!","Good one!"],
+    [
+      "kiss me",
+      "cutiee",
+      "dc is the best!"
+    ],
+    ["here's a joke to cheer you up. Type joke below."],
+    ["Why did the scarecrow win an award? Because he was outstanding in his field."],
+    [
+      "What has a bed that you can’t sleep in? A river",
+      "Why were the teacher’s eyes crossed? She couldn’t control her pupils.",
+      "What starts with E, ends with E, and has only 1 letter in it? An Envelope",
+      "How does the ocean say hello? It waves",
+      "What lights up a soccer stadium? A soccer match",
+      "What creature is smarter than a talking parrot? A spelling bee",
+      "Which U.S. state has the smallest soft drinks? Minnesota (as in, “mini-soda”)"
+    ]
+    
+
+
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening...",
+    "I don't understand :/"
+  ]
+  
+  // Whatever else you want :)
+  
+  const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
